18611521eff6fa2605d8527fad8d847a9fe10247
Vivek Patel <dotnetguts@gmail.com>
2020-06-21 22:19:31 -0400

    Added Configuration values for testing

--- FirstAspNetCore31/Controllers/HomeController.cs
+++ FirstAspNetCore31/Controllers/HomeController.cs
@@ -6,20 +6,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FirstAspNetCore31.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace FirstAspNetCore31.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            var test1 = configuration["Test1"];
+            ViewBag.Test1Value = test1;
+
+            var test2 = configuration["Test2"];
+            ViewBag.Test2Value = test2;
+
             return View();
         }
 
